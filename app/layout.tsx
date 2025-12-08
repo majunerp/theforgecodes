24c30a9d8dc380bb9e8639721da55ec4fe56c4a3
majunerp <majunerperp@gmail.com>
2025-12-08 13:56:24 +0800

    Add Google tag manager snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,19 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N0TG1GD3WX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N0TG1GD3WX');
+`,
+          }}
+        />
         <link rel="canonical" href="https://theforgecodes.org/" />
       </head>
       <body
