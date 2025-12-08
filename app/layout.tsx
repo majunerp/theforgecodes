1cc9cf410d67191220f1f4973ac52ea751846f55
majunerp <majunerperp@gmail.com>
2025-12-08 14:22:28 +0800

    Add Microsoft Clarity tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,18 @@ export default function RootLayout({
 `,
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ui69ciwae1");
+`,
+          }}
+        />
         <link rel="canonical" href="https://theforgecodes.org/" />
       </head>
       <body
